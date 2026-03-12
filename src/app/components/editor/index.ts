bfa7b7fc8b924eebc16ede2b283c68d004faafaa
Fernando Branco <fernandoferrerobranco@gmail.com>
2026-03-12 19:08:27 -0300

    delete last version

--- src/app/components/editor/index.ts
+++
@@ -1,6 +0,0 @@
-// Editor Components
-export { EditableWrapper } from './EditableWrapper';
-export { EditorModal } from './EditorModal';
-export { EditorToolbar } from './EditorToolbar';
-export { EditableHeroSection } from './EditableHeroSection';
-export { EditableSkillsSection } from './EditableSkillsSection';
